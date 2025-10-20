27ac80930ae1e92a10a99b7359d876a6d940ec7a
mis <ajaym@unicourt.com>
2025-10-20 15:12:31 +0530

    Remove unused CasesLayoutComponent import from custom list

--- src/app/pages/custom-list/custom-list.component.ts
+++ src/app/pages/custom-list/custom-list.component.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { ActivatedRoute, Router } from '@angular/router';
 import { SideNavComponent } from '../../components/navigation/side-nav/side-nav.component';
 import { AppHeaderComponent } from '../../layouts/app-header/app-header.component';
-import { CasesLayoutComponent } from '../../layouts/cases-layout/cases-layout.component';
 import { CustomListLayoutComponent } from '../../layouts/custom-list-layout/custom-list-layout.component';
 
 @Component({
@@ -13,7 +12,6 @@ import { CustomListLayoutComponent } from '../../layouts/custom-list-layout/cust
     CommonModule, 
     SideNavComponent, 
     AppHeaderComponent,
-    CasesLayoutComponent,
     CustomListLayoutComponent
   ],
   templateUrl: './custom-list.component.html',
